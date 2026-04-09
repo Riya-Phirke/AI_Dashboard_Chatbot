d634484f401df5353c5a90bf77d6d5eda51a91d5
Riya-Phirke <phirkeriya@gmail.com>
2026-04-09 20:54:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 import os
 from groq import Groq
 
+import os
+os.environ["PORT"] = "10000"
+
 # Load API key
 load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
